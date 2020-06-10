4d1a650ecb4af722d856da5665b4ca93db067c9e
Triangleyj <dpwns99@gmail.com>
2020-06-10 18:15:51 +0900

    Fix gitignore

--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -3,6 +3,7 @@
         <h3>회원가입</h3>
         <input type="text" v-model="email" placeholder="email"><br>
         <input type="password" v-model="password" placeholder="password"><br>
+        <input type="password" v-model="password_ckeck" placeholder="re-enter password"><br>
         <button @click="signUp()">가입하기</button>
         <br>
         <button @click="gotoLogin()" >Go to Login</button>
@@ -17,7 +18,8 @@
         data(){
             return{
                 email: '',
-                password: ''
+                password: '',
+                password_ckeck: ''
             }
         },
         methods:{
@@ -29,8 +31,17 @@
                 const regex_email = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
                 if(this.email.match(regex_email) == null || this.password.match(regex_pw) == null){
-                    alert("올바른 이메일 또는 패스워드(최소 8자, 영문+숫자)를 입력해 주십시오.")
-                } else {
+                    alert("올바른 이메일 또는 패스워드(최소 8자, 영문+숫자)를 입력해 주십시오.");
+                    if(this.email.match(regex_email) == null) this.email = '';
+                    if(this.password.match(regex_pw) == null){
+                        this.password = '';
+                        this.password_ckeck = '';
+                    }
+                } else if(this.password !== this.password_ckeck){
+                    alert("비밀번호가 다릅니다. 다시 확인해 주세요.");
+                    this.password = '';
+                    this.password_ckeck = '';
+                }else {
                     axios.post('http://localhost:5000/signup', {id: this.email, pw: this.password}).then(result => {
                         let code =result.data.result;
                         if (code === 1) {
