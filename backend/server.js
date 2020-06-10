48d6be64b439ecf1ca3e09dea37b28dee8fe4b2f
Triangleyj <dpwns99@gmail.com>
2020-06-10 17:35:38 +0900

    Implement signup system.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,40 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const CryptoJS = require('crypto-js');
+
+const Schema = mongoose.Schema;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// CONNECT TO MONGODB SERVER
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://localhost/mongodb_urlshortner', { useNewUrlParser: true , useUnifiedTopology: true} );
+const userSchema = new Schema({
+    id: String,
+    pw: String
+});
+const User = mongoose.model('user', userSchema);
+
+
 
 
 let getURLFromCode = code => {
 //TODO: searching algorithm
 };
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
@@ -17,6 +46,35 @@ app.get('/:id', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+
+});
+
+app.post('/signup', (req, res) => {
+    let id = req.body.id;
+    let pw = req.body.pw;
+    let user = new User({
+        id: id,
+        pw: CryptoJS.MD5(pw)
+    });
+
+    User.findOne({id: id}, (err, doc) => {
+        if(doc === null){
+            user.save((err, user) => {
+                if(err){
+                    res.json({result: -1});
+                    return console.error(err);
+                }
+
+                res.json({result: 1});
+            });
+        }
+        else res.json({result: 0});
+    });
+
+
+
+
+
 
 });
 
--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -3,23 +3,48 @@
         <h3>회원가입</h3>
         <input type="text" v-model="email" placeholder="email"><br>
         <input type="password" v-model="password" placeholder="password"><br>
-        <button>가입하기</button>
+        <button @click="signUp()">가입하기</button>
         <br>
         <button @click="gotoLogin()" >Go to Login</button>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         name: "SignUp",
         data(){
             return{
+                email: '',
+                password: ''
             }
         },
         methods:{
             gotoLogin(){
                 this.$emit('input', 'p/login');
             },
+            signUp(){
+                const regex_pw = /^[A-Za-z0-9!@#$%^&+=]{8,}$/;
+                const regex_email = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+                if(this.email.match(regex_email) == null || this.password.match(regex_pw) == null){
+                    alert("올바른 이메일 또는 패스워드(최소 8자, 영문+숫자)를 입력해 주십시오.")
+                } else {
+                    axios.post('http://localhost:5000/signup', {id: this.email, pw: this.password}).then(result => {
+                        let code =result.data.result;
+                        if (code === 1) {
+                            alert("성공적으로 회원가입이 완료되었습니다. 다시 로그인해주세요,");
+                            this.gotoLogin();
+                        } else if(code === 0) {
+                            alert("이미 존재하는 이메일입니다.");
+                        } else {
+                            alert(`오류가 발생했습니다. 관리자에게 문의해 주십시오. 오류 코드 : ${code} | ${result.status}`);
+                        }
+
+                    });
+                }
+            }
         },
     }
 </script>
